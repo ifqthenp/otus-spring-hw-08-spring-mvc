78b1676b7e1965a606206f6ed35ab0377e9ba778
Andrei Bogomja <bogomja@gmail.com>
2019-03-02 20:56:56 +0000

    Add library service bean

--- src/main/java/com/otus/hw_08/controllers/LibraryController.java
+++ src/main/java/com/otus/hw_08/controllers/LibraryController.java
@@ -1,11 +1,16 @@
 package com.otus.hw_08.controllers;
 
+import com.otus.hw_08.services.LibraryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class LibraryController {
+
+    private final LibraryService libraryService;
 
     @GetMapping("/library/add")
     public String add() {

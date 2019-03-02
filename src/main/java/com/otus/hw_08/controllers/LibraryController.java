80d43a0926e3541a034bde77a969461d677d2398
Andrei Bogomja <bogomja@gmail.com>
2019-03-02 16:48:27 +0000

    Add request parameters for search

--- src/main/java/com/otus/hw_08/controllers/LibraryController.java
+++ src/main/java/com/otus/hw_08/controllers/LibraryController.java
@@ -2,6 +2,7 @@ package com.otus.hw_08.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LibraryController {
@@ -12,7 +13,9 @@ public class LibraryController {
     }
 
     @GetMapping("/library/books/search")
-    public String find() {
+    public String findBook(@RequestParam(value = "title", required = false) final String title,
+                           @RequestParam(value = "author", required = false) final String author,
+                           @RequestParam(value = "genre", required = false) final String genre) {
         return "find_book";
     }
 

a70f5f518ded395966aff661b49408f11dcf1183
Andrei Bogomja <bogomja@gmail.com>
2019-03-04 15:38:11 +0000

    Add javadoc

--- src/main/java/com/otus/hw_08/repository/projections/BookProjection.java
+++ src/main/java/com/otus/hw_08/repository/projections/BookProjection.java
@@ -1,8 +1,13 @@
 package com.otus.hw_08.repository.projections;
 
+import com.otus.hw_08.controllers.LibraryController;
+import org.springframework.ui.Model;
+
 /**
  * This projection of book is to be used in search result
  * initiated by user in book search form.
+ *
+ * @see LibraryController#resultBook(Model, String, String, String)
  */
 public interface BookProjection {
 
